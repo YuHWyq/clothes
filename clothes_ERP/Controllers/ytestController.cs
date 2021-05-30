c34baf380a3e407209c807e537af77f8d6602ed1
1971114079@qq.com <1971114079@qq.com>
2021-05-30 16:17:23 +0800

    ytest

---
+++ clothes_ERP/Controllers/ytestController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace clothes_ERP.Controllers
+{
+    public class ytestController : Controller
+    {
+        // GET: ytest
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
